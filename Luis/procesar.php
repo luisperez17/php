e5e391a696cac28610dab166fbe8a358de3e5ce3
Luis Eduardo Perez <luis.perez@esinergia.co>
2024-04-16 08:45:17 -0500

    Ejercicio guardar informacion en db

--- Luis/procesar.php
+++ Luis/procesar.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <title>Procesamiento PHP</title>
-</head>
-<body>
-    <h2>Procesamiento PHP</h2>
-    <?php
+<?php
+
+    include 'modelo/modelo.php';
+
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $edad = $_POST['edad'];
@@ -13,12 +9,7 @@
     $preferencias = $_POST['preferencias'];
     $estado_civil = $_POST['estado_civil'];
 
-    echo "<p>Nombre: $nombre</p>";
-    echo "<p>email: $email</p>";
-    echo "<p>edad: $edad</p>";
-    echo "<p>genero: $genero</p>";
-    echo "<p>preferencias: $preferencias</p>";
-    echo "<p>estado_civil: $estado_civil</p>";
-    ?>
-</body>
-</html>
+    guardarDatos($nombre, $email, $edad, $genero, $preferencias, $estado_civil);
+
+    header("Location: /vista/vista.php");
+?>
