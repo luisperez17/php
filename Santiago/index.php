c3b892980188e8eb848a679024dedf02632077f5
santiagoroasan <santiagoroasan@PORT-SANTIROASA.unisabana.edu.co>
2024-04-18 01:06:02 -0500

    Creacion de formulario principal

---
+++ Santiago/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Formulario PHP</title>
+</head>
+<body>
+    <h2>Formulario PHP</h2>
+    <form>
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre" required><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required><br>
+
+        <label for="edad">Edad:</label>
+        <input type="number" name="edad" id="edad" required><br>
+
+        <input type="submit" value="Enviar">
